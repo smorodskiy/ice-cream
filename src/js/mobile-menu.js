557b76218bc35d17d5c24bceab0b5a6cca872745
Igor Smorodsky <ingerard@gmail.com>
2022-08-17 12:09:56 +0300

    fixed header

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,39 +1,76 @@
 (() => {
-  // Scroll size
-  document.documentElement.style.setProperty(
-    '--scrollbar-width',
-    window.innerWidth - document.documentElement.clientWidth + 'px'
-  );
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
 
-  const menuBtnsRef = document.querySelectorAll('[data-menu-button]');
-  const mobileMenuRef = document.querySelector('[data-menu]');
+  menuBtnRef.addEventListener("click", () => {
+    const expanded =
+      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
-  for (const btn in menuBtnsRef) {
-    if (Object.hasOwnProperty.call(menuBtnsRef, btn)) {
-      const element = menuBtnsRef[btn];
-      element.addEventListener('click', () => {
-        const expanded =
-          element.getAttribute('aria-expanded') === 'true' || false;
-        element.classList.toggle('is-open');
-        element.setAttribute('aria-expanded', !expanded);
-        mobileMenuRef.classList.toggle('is-open');
-      });
-    }
-  }
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
 
-  // Close the mobile menu on wider screens if the device orientation changes
-  window.matchMedia('(min-width: 1199px)').addEventListener('change', e => {
-    if (!e.matches) return;
-    mobileMenuRef.classList.remove('is-open');
-
-    for (const btn in menuBtnsRef) {
-      if (Object.hasOwnProperty.call(menuBtnsRef, btn)) {
-        const element = menuBtnsRef[btn];
-        element.setAttribute('aria-expanded', false);
-        element.classList.remove('is-open');
-      }
-    }
-
-    // bodyScrollLock.enableBodyScroll(document.body);
+      mobileMenuRef.classList.toggle("is-open"); 
   });
+    
+  // Close the mobile menu on wider screens if the device orientation changes
+     window.matchMedia('(min-width: 1199px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+    });
 })();
+
+// (() => {
+//   const menuBtnsRef = document.querySelectorAll('[data-menu-button]');
+//   const mobileMenuRef = document.querySelector('[data-menu]');
+
+//   for (const btn in menuBtnsRef) {
+//     if (Object.hasOwnProperty.call(menuBtnsRef, btn)) {
+//       const element = menuBtnsRef[btn];
+//       console.log(element);
+
+//       element.addEventListener('click', () => {
+//         toogleClass();
+//       });
+//     }
+//   }
+
+//   // Close the mobile menu on wider screens if the device orientation changes
+//   window.matchMedia('(min-width: 1199px)').addEventListener('change', e => {
+//     if (!e.matches) return;
+//     mobileMenuRef.classList.remove('is-open');
+
+//     closeModal();
+
+//     // bodyScrollLock.enableBodyScroll(document.body);
+//   });
+
+//   function closeModal() {
+//     console.log('close');
+//     for (const btn in menuBtnsRef) {
+//       if (Object.hasOwnProperty.call(menuBtnsRef, btn)) {
+//         const element = menuBtnsRef[btn];
+//         console.log(element);
+//         element.setAttribute('aria-expanded', false);
+//         element.classList.remove('is-open');
+//       }
+//     }
+//   }
+
+//   function toogleClass() {
+    
+//     for (const btn in menuBtnsRef) {
+//       if (Object.hasOwnProperty.call(menuBtnsRef, btn)) {
+//         const element = menuBtnsRef[btn];
+//         const expanded =
+//         element.getAttribute('aria-expanded') === 'true' || false;
+//         element.classList.toggle('is-open');
+//         element.setAttribute('aria-expanded', !expanded);
+//       }
+//     }
+
+//     mobileMenuRef.classList.toggle('is-open');
+//   }
+
+// })();
